0b7acf5c5a9a47cd1c28dd7facfbd8ccceb9e218
Jessica Barnitz <Jessica.Barnitz@colorado.edu>
2017-02-25 21:38:21 -0700

    I updated the outline

--- index.php
+++ index.php
@@ -22,13 +22,10 @@ html,body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
   <div class="w3-container w3-row">
     <div class="w3-col s4">
       <!--<img src="/w3images/avatar2.png" class="w3-circle w3-margin-right" style="width:46px">/-->
-       <a href="#"><i class="fa fa-user fa-fw"></i></a>
+       <a href="#"><i class="fa fa-user w3-xxxlarge"></i></a>
     </div>
     <div class="w3-col s8">
       <span>Welcome, <strong>Lady Jane</strong></span><br>
-      <a href="#" class="w3-hover-none w3-hover-text-red w3-show-inline-block"><i class="fa fa-envelope"></i></a>
-      <a href="#" class="w3-hover-none w3-hover-text-green w3-show-inline-block"><i class="fa fa-user"></i></a>
-      <a href="#" class="w3-hover-none w3-hover-text-blue w3-show-inline-block"><i class="fa fa-cog"></i></a>
     </div>
   </div>
   <hr>
@@ -58,6 +55,55 @@ html,body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
   <header class="w3-container" style="padding-top:22px">
     <h5><b><i class="fa fa-dashboard"></i> My Dashboard</b></h5>
   </header>
+	<div class="w3-row-padding w3-margin-bottom">
+	    <div class="w3-quarter">
+
+      </div>
+    </div> 
+
+  <div class="w3-row-padding w3-margin-bottom">
+    <div class="w3-quarter">
+      <div class="w3-container w3-purple w3-padding-16">
+        <div class="w3-left"><i class="fa fa-balance-scale w3-xxxlarge"></i></div>
+        <div class="w3-right">
+          <h3>52</h3>
+        </div>
+        <div class="w3-clear"></div>
+        <h4>Retirement Plan</h4>
+      </div>
+    </div>
+    <div class="w3-quarter">
+      <div class="w3-container w3-indigo w3-padding-16">
+        <div class="w3-left"><i class="fa fa-industry w3-xxxlarge"></i></div>
+        <div class="w3-right">
+          <h3>99</h3>
+        </div>
+        <div class="w3-clear"></div>
+        <h4>Personal Portfolio</h4>
+      </div>
+    </div>
+    <div class="w3-quarter">
+      <div class="w3-container w3-teal w3-padding-16">
+        <div class="w3-left"><i class="fa fa-database w3-xxxlarge"></i></div>
+        <div class="w3-right">
+          <h3>23</h3>
+        </div>
+        <div class="w3-clear"></div>
+        <h4>Draft a Portfolio</h4>
+      </div>
+    </div>
+    <div class="w3-quarter">
+      <div class="w3-container w3-light-blue w3-text-white w3-padding-16">
+        <div class="w3-left"><i class="fa fa-pie-chart w3-xxxlarge"></i></div>
+        <div class="w3-right">
+          <h3>50</h3>
+        </div>
+        <div class="w3-clear"></div>
+        <h4>League Portfolio</h4>
+      </div>
+    </div>
+  </div>
+
   <div class="w3-row-padding w3-margin-bottom">
     <div class="w3-quarter">
 		<?php
@@ -103,50 +149,7 @@ html,body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
 
 		?>
       </div>
-    </div>
-  <div class="w3-row-padding w3-margin-bottom">
-    <div class="w3-quarter">
-      <div class="w3-container w3-purple w3-padding-16">
-        <div class="w3-left"><i class="fa fa-comment w3-xxxlarge"></i></div>
-        <div class="w3-right">
-          <h3>52</h3>
-        </div>
-        <div class="w3-clear"></div>
-        <h4>Messages</h4>
-      </div>
-    </div>
-    <div class="w3-quarter">
-      <div class="w3-container w3-indigo w3-padding-16">
-        <div class="w3-left"><i class="fa fa-eye w3-xxxlarge"></i></div>
-        <div class="w3-right">
-          <h3>99</h3>
-        </div>
-        <div class="w3-clear"></div>
-        <h4>Views</h4>
-      </div>
-    </div>
-    <div class="w3-quarter">
-      <div class="w3-container w3-teal w3-padding-16">
-        <div class="w3-left"><i class="fa fa-share-alt w3-xxxlarge"></i></div>
-        <div class="w3-right">
-          <h3>23</h3>
-        </div>
-        <div class="w3-clear"></div>
-        <h4>Shares</h4>
-      </div>
-    </div>
-    <div class="w3-quarter">
-      <div class="w3-container w3-light-blue w3-text-white w3-padding-16">
-        <div class="w3-left"><i class="fa fa-users w3-xxxlarge"></i></div>
-        <div class="w3-right">
-          <h3>50</h3>
-        </div>
-        <div class="w3-clear"></div>
-        <h4>Users</h4>
-      </div>
-    </div>
-  </div>
-
+	</div>
 <!--   <div class="w3-container w3-section">
     <div class="w3-row-padding" style="margin:0 -16px">
       <div class="w3-third">
